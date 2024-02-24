15c4ca64e457d05996f05aa5fee85afa66bdbd7c
Swarga-codes <sd3526@srmist.edu.in>
2024-02-25 01:49:56 +0530

    fix:context.ts

--- app/lib/context.ts
+++ app/lib/context.ts
@@ -1,7 +1,7 @@
 import { createContext } from "react";
 
- const welcomePageContext=createContext()
- const currentChatContext=createContext()
+ const welcomePageContext=createContext<any>(null)
+ const currentChatContext=createContext<any>(null)
 
 
  export {welcomePageContext,currentChatContext}
