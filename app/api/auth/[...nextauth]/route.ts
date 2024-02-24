e68dc06010bbec955e81648f345670a190c5114c
Swarga-codes <sd3526@srmist.edu.in>
2024-02-25 01:14:13 +0530

    fix:export authOptions

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions={
+ const authOptions={
   providers:[GoogleProvider({
     clientId:process.env.AUTH_GOOGLE_ID??"",
     clientSecret:process.env.AUTH_GOOGLE_SECRET??""
