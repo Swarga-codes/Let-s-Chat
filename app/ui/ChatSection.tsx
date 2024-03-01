9c918b5ac884c27e8c42eda6e84421060e421f13
Swarga-codes <sd3526@srmist.edu.in>
2024-03-01 20:18:40 +0530

    fix: socket url

--- app/ui/ChatSection.tsx
+++ app/ui/ChatSection.tsx
@@ -19,7 +19,7 @@ function ChatSection() {
 
 
   useEffect(() => {
-   socket = io(process.env.API_KEY || "")
+   socket = io("https://let-s-chat.onrender.com")
     socket.emit('setup', currentChat.participants.find((participant:Participants) => participant.email === session?.user?.email));
     socket.on('connection', () => setIsSocketConnected(true));
 },[currentChat.participants,session?.user?.email]);
