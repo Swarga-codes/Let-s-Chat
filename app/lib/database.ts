2335d85be748e48ed7df8c922ccfeb09d6f34ee4
Swarga-codes <sd3526@srmist.edu.in>
2024-03-01 19:26:16 +0530

    removed unnecessary code snippets

--- app/lib/database.ts
+++ app/lib/database.ts
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 const connectDB=async()=>{
     try{
-        // await mongoose.connect("mongodb+srv://swargarajd:chatapp123@cluster0.ikdkqq5.mongodb.net/?retryWrites=true&w=majority")
         await mongoose.connect(process.env.DB_HOST|| "")
     }
     catch(err){
