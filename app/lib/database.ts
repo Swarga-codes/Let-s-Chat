eb720f8031bcc8a6305c76320b1e9c3334166124
Swarga-codes <sd3526@srmist.edu.in>
2024-03-01 19:23:00 +0530

    updated db url

--- app/lib/database.ts
+++ app/lib/database.ts
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB=async()=>{
     try{
-        await mongoose.connect("mongodb+srv://swargarajd:chatapp123@cluster0.ikdkqq5.mongodb.net/?retryWrites=true&w=majority")
+        // await mongoose.connect("mongodb+srv://swargarajd:chatapp123@cluster0.ikdkqq5.mongodb.net/?retryWrites=true&w=majority")
+        await mongoose.connect(process.env.DB_HOST|| "")
     }
     catch(err){
         console.log('Could not establish connection to Database!',err)
