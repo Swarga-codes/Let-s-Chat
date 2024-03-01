ee54c080d12470d430bd1f0b5a01e27533d59dac
Swarga-codes <sd3526@srmist.edu.in>
2024-03-01 19:11:52 +0530

    updated env variable for db

--- app/lib/database.ts
+++ app/lib/database.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB=async()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URI || "")
+        await mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URI || "")
     }
     catch(err){
         console.log('Could not establish connection to Database!',err)
